9ed10caca4dd9690b6039127e36936b2588201c8
KHT369 <ktsao@chicagobooth.edu>
2024-10-25 23:32:10 +0000

    save 2

--- app.rb
+++ app.rb
@@ -8,15 +8,6 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  @square = params.fetch("number", 0).to_i
-  @results = []
-  
-  @square.times do
-    squared = @square * @square
-    
-    @results.push(squared)
-  end
-
   erb(:homepage)
 end
 
@@ -25,5 +16,47 @@ get("/square/new") do
 end
 
 get("/square/results") do
+  @number = params.fetch("number", 0).to_f
+  @result = @number * @number
   erb(:results)
+end
+
+get("/square_root/new") do
+  erb(:homepageroot)
+end
+
+get("/square_root/results") do
+  @number = params.fetch("number", 0).to_f
+  @resultsqrt = Math.sqrt(@number)
+  erb(:resultsqrt)
+end
+
+get("/payment/new") do
+  erb(:homepagepay)
+end
+
+get("/payment/results") do
+  @apr = params.fetch("user_apr", 0).to_f
+  @pv = params.fetch("user_pv", 0).to_f
+  @years = params.fetch("user_years", 0).to_i
+  
+  ptop = (@apr/ 100.0 / 12) * @pv
+  pbottom = (1 - ((1 + @apr / 100.0 / 12) ** (-@years * 12))
+  @aprp = "#{@apr / 100.0}%"
+
+  @payment = ptop / pbottom
+
+  erb(:resultspay)
+end
+
+get("/random/new") do
+  erb(:homepagerand)
+end
+
+get("/random/results") do
+  @min = params.fetch("user_min").to_f
+  @max = params.fetch("user_max").to_f
+
+  @resultrand = rand(@min...@max)
+  erb(:resultsrand)
 end
