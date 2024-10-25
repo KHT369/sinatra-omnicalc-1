c3171bb3d89658c58900df81e5c6d391f7747291
KHT369 <ktsao@chicagobooth.edu>
2024-10-25 22:15:08 +0000

    save 1

--- app.rb
+++ app.rb
@@ -1,9 +1,29 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @square = params.fetch("number", 0).to_i
+  @results = []
+  
+  @square.times do
+    squared = @square * @square
+    
+    @results.push(squared)
+  end
+
+  erb(:homepage)
+end
+
+get("/square/new") do
+  erb(:homepage)
+end
+
+get("/square/results") do
+  erb(:results)
 end
